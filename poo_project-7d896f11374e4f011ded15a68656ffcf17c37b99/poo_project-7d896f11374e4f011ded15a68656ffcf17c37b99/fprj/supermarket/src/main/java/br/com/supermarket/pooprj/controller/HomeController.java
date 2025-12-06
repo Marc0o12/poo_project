929aa739dd11047956e6903efc996a12feca6cee
Marc0o12 <161870231+Marc0o12@users.noreply.github.com>
2025-12-06 19:37:57 -0300

    Adicionei o spring security

--- poo_project-7d896f11374e4f011ded15a68656ffcf17c37b99/poo_project-7d896f11374e4f011ded15a68656ffcf17c37b99/fprj/supermarket/src/main/java/br/com/supermarket/pooprj/controller/HomeController.java
+++ poo_project-7d896f11374e4f011ded15a68656ffcf17c37b99/poo_project-7d896f11374e4f011ded15a68656ffcf17c37b99/fprj/supermarket/src/main/java/br/com/supermarket/pooprj/controller/HomeController.java
@@ -1,6 +1,7 @@
 package br.com.supermarket.pooprj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,4 +11,20 @@ public class HomeController {
     public String home() {
         return "user/home"; // nome do template sem extensão
     }
+    @GetMapping({"/login"})
+	public String login() {
+		
+		return "login";
+	}	
+	
+	// login invalido
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais inválidas!");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+		return "login";
+	}	
+  
 }
